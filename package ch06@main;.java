ff12cb897de5c872c13e3613c18ed8a7ac32e89f
parkyouhyun <151006383+parkyouhyu@users.noreply.github.com>
2024-10-17 14:50:17 +0900

    Add files via upload

---
+++ package ch06@main;.java
@@ -0,0 +1,13 @@
+package ch06@main;
+public class Main {
+
+    
+    public static void main(String[] args) {
+        Car car1 = new Car("Hyundal", "Sonata",100,0);
+        car1.ShowInfo();
+        car1.AddSpeed(40);
+
+        car.showInfo();
+        car.AddSpeed(50);
+    }
+}
