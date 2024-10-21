ca8a11ad5fe6544934cbbc9b1da360f5fafd92cd
parkyouhyun <151006383+parkyouhyu@users.noreply.github.com>
2024-10-21 22:35:48 +0900

    Add files via upload

---
+++ package example;.java
@@ -0,0 +1,13 @@
+package example;
+public class FlaotExample {
+    public static void main(String[] args) {
+        float x;
+        float y = 1.0f;
+        for ( x = 0.1f; x<=1.0f; x+= 0.1f) {  
+        }
+                System.out.printIn("x와y는같음");
+        } else {
+                System.out.printIn("x와y는다름");
+        }
+    }
+}
