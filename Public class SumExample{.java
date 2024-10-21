0602008adcc5fa0f04c669a4961b2df80ab91d5c
parkyouhyun <151006383+parkyouhyu@users.noreply.github.com>
2024-10-22 00:43:11 +0900

    Add files via upload

---
+++ Public class SumExample{.java
@@ -0,0 +1,9 @@
+Public class SumExample{
+    Public static void main(String[] args){
+        int sum = 0;
+        for(int i=1,i<=100; i++) {
+            sum = sum + i;
+            System.out.printIn("1~100의 합:"+sum);
+        }
+    }
+}
